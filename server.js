4272deeb02972479d9bf50e9ce2afce516a2bac9
sbxinth <son.son.2013@hotmail.co.th>
2022-03-10 01:01:01 +0700

    test1

--- server.js
+++ server.js
@@ -10,10 +10,25 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // 
-
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "swufinal"
+});
+//"SELECT * FROM Account WHERE username = ? AND password = ?",[username, password],
 //
 app.get("/home", function(request, response) {
   response.sendFile(path.join(__dirname, '/public/html/swu.html'));
+ // start connect database 
+connection.connect();
+connection.query('SELECT * FROM account WHERE username = ? AND password = ?',["co611010035", "co611010035"], 
+function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+connection.end();
+// end connect database
  });
  app.get("/login", function(request, response) {
   response.sendFile(path.join(__dirname, '/public/html/cosci_login.html'));
