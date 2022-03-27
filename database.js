a75f78b59fab8442e2f5bdb23bdfb9ea2adb7ae1
sbxinth <son.son.2013@hotmail.co.th>
2022-03-27 16:23:36 +0700

    update database

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ var dbConnectionn = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "thesis"
+    database: "newthesis"
   });
 
 module.exports = dbConnectionn
