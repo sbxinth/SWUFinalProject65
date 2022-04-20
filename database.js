08569404a22e575c2ff0c607757ede3d2794114b
sbxinth <son.son.2013@hotmail.co.th>
2022-04-20 08:36:05 +0700

    fix little admin

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ var dbConnectionn = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "thesisz"
+    database: "realrealdb"
   });
 
 module.exports = dbConnectionn
